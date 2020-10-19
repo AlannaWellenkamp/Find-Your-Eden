c5b2e6ba395e0b0d21712efc53befa1ec7e44885
Alanna Wellenkamp <actuallyalanna@gmail.com>
2020-10-19 11:05:30 -0700

    implemented fetching and full city info list construction

--- main.js
+++ main.js
@@ -1,53 +1,76 @@
 'use: strict';
 
-let scores = [];
-const importance = [1, 2, 1, 1, 1, 2, 1, 2, 0, 1];
-const cityList = {};
+let fetched = 0;
+let matchOptions = { housing: '1', costOfLiving: '1', travelConnectivity: '1', commute: '1', safety: '1', healthcare: '1', education: '1', environmentalQuality: '1', taxation: '1', leisureAndCulture: '1' };
+let cityList = [];
+const cities = ['albuquerque', 'anchorage', 'asheville', 'atlanta', 'austin', 'baltimore', 'birmingham-al', 'boise', 'boston', 'boulder', 'buffalo', 'charleston', 'charlotte', 'chattanooga', 'chicago', 'cincinnati', 'cleveland', 'colorado-springs', 'columbus', 'dallas', 'denver', 'des-moines', 'detroit', 'eugene', 'fort-collins', 'houston', 'indianapolis', 'jacksonville', 'kansas-city', 'knoxville', 'las-vegas', 'los-angeles', 'madison', 'memphis', 'miami', 'milwaukee', 'minneapolis-saint-paul', 'nashville', 'new-orleans', 'new-york', 'oklahoma-city', 'omaha', 'orlando', 'philadelphia', 'phoenix', 'pittsburgh', 'portland-me', 'portland-or', 'raleigh', 'richmond', 'rochester', 'salt-lake-city', 'san-antonio', 'san-diego', 'san-francisco-bay-area', 'san-juan', 'san-luis-obispo', 'st-louis', 'tampa-bay-area', 'washington-dc'];
+let scores = {};
+let scoresSorted = [];
+
 
 function cityLoop() {
-    const cities = ['albuquerque', 'anchorage', 'asheville', 'atlanta', 'austin', 'baltimore', 'birmingham-al', 'boise', 'boston', 'boulder', 'buffalo', 'charleston', 'charlotte', 'chattanooga', 'chicago', 'cincinnati', 'cleveland', 'colorado-springs', 'columbus', 'dallas', 'denver', 'des-moines', 'detroit', 'eugene', 'fort-collins', 'houston', 'indianapolis', 'jacksonville', 'kansas-city', 'knoxville', 'las-vegas', 'los-angeles', 'madison', 'memphis', 'miami', 'milwaukee', 'minneapolis-saint-paul', 'nashville', 'new-orleans', 'new-york', 'oklahoma-city', 'omaha', 'orlando', 'philadelphia', 'phoenix', 'pittsburgh', 'portland-me', 'portland-or', 'raleigh', 'richmond', 'rochester', 'salt-lake-city', 'san-antonio', 'san-diego', 'san-francisco-bay-area', 'san-juan', 'san-luis-obispo', 'st-louis', 'tampa-bay-area', 'washington-dc'];
-    console.log('launching city loop,' + cities.length);
+    console.log(cities);
     for (let i = 0; i < cities.length; i++) {
         fetch(`https://api.teleport.org/api/urban_areas/slug:${cities[i]}/scores/`)
             .then(response => response.json())
-            .then(responseJson => buildCityList(cities[i], responseJson));
+            .then(responseJson => buildCityList(cities[i], responseJson))
+
     }
-    console.log(cityList);
 }
 
 function buildCityList(city, responseJson) {
-    cityList[city] = responseJson;
+    const currentCity = {
+        name: city,
+        housing: responseJson.categories[0].score_out_of_10,
+        costOfLiving: responseJson.categories[1].score_out_of_10,
+        travelConnectivity: responseJson.categories[4].score_out_of_10,
+        commute: responseJson.categories[5].score_out_of_10,
+        safety: responseJson.categories[7].score_out_of_10,
+        healthcare: responseJson.categories[8].score_out_of_10,
+        education: responseJson.categories[9].score_out_of_10,
+        environmentalQuality: responseJson.categories[10].score_out_of_10,
+        taxation: responseJson.categories[12].score_out_of_10,
+        leisureAndCulture: responseJson.categories[14].score_out_of_10
+    };
+    cityList.push(currentCity);
+    if (cityList.length === 60) {
+        console.log(cityList);
+        calculateScore(cityList, cities, matchOptions);
+    }
 
 }
 
 
 
-function calculateScore(responseJson) {
-    console.log(responseJson);
-    let score = 0;
-    console.log('calculating score');
-    console.log(responseJson.categories.length + ' categories');
-    score += (responseJson.categories[0].score_out_of_10) * (importance[0]);
-    score += (responseJson.categories[1].score_out_of_10) * (importance[1]);
-    score += (responseJson.categories[4].score_out_of_10) * (importance[2]);
-    score += (responseJson.categories[5].score_out_of_10) * (importance[3]);
-    score += (responseJson.categories[7].score_out_of_10) * (importance[4]);
-    score += (responseJson.categories[8].score_out_of_10) * (importance[5]);
-    score += (responseJson.categories[9].score_out_of_10) * (importance[6]);
-    score += (responseJson.categories[10].score_out_of_10) * (importance[7]);
-    score += (responseJson.categories[12].score_out_of_10) * (importance[8]);
-    score += (responseJson.categories[14].score_out_of_10) * (importance[9]);
-    console.log('calculated score');
-    console.log(score);
-    scores.push(score);
+function calculateScore(cityList, cities, matchOptions) {
+    console.log('calculating scores');
+    for (let i = 0; i < cities.length; i++) {
+        console.log('calculating score for ' + cities[i]);
+        let currentCity = {}; 
+        currentCity = cityList.find(o => o.name === cities[i]);
+        let score = 0;
+        score += (currentCity.housing) * (matchOptions.housing);
+        score += (currentCity.costOfLiving) * (matchOptions.costOfLiving);
+        score += (currentCity.travelConnectivity) * (matchOptions.travelConnectivity);
+        score += (currentCity.commute) * (matchOptions.commute);
+        score += (currentCity.safety) * (matchOptions.safety);
+        score += (currentCity.healthcare) * (matchOptions.healthcare);
+        score += (currentCity.education) * (matchOptions.education);
+        score += (currentCity.environmentalQuality) * (matchOptions.environmentalQuality);
+        score += (currentCity.taxation) * (matchOptions.taxation);
+        score += (currentCity.leisureAndCulture) * (matchOptions.leisureAndCulture);
+        scores[cities[i]] = score;
+    }
     console.log(scores);
-
+    var scoresSorted = Object.keys(scores).sort(function(a,b){return scores[b]-scores[a]});
+    console.log(scoresSorted);
 }
 
 
 function runApp() {
     console.log('app running');
     cityLoop();
+    
 
 }
 
@@ -60,10 +83,10 @@ function runApp() {
     }
 }*/
 
+runApp();
+
+/*
 $(runApp);
-
-
-
-
+*/
 
 
